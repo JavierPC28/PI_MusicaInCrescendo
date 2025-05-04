a234ae03987065057e7bb5eeb7d8199f9e548bbb
JavierPC28 <135653311+JavierPC28@users.noreply.github.com>
2025-05-04 13:54:37 +0200

    v1.3.2 (Realtime Dtabase conectado)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
+    implementation(libs.firebase.database)
 
     // Tests
     testImplementation(libs.junit)
